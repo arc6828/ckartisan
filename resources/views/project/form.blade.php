57116da969343a205381cbc598b8ea106ae37c30
arc6828 <chavalit.kow@gmail.com>
2019-09-16 12:10:50 +0700

    Update form.blade.php

--- resources/views/project/form.blade.php
+++ resources/views/project/form.blade.php
@@ -10,7 +10,7 @@
 </div>
 <div class="form-group {{ $errors->has('begin_date') ? 'has-error' : ''}}">
     <label for="begin_date" class="control-label">{{ 'Begin Date' }}</label>
-    <input class="form-control" name="begin_date" type="date" id="begin_date" value="{{ isset($project->begin_date) ? $project->begin_date : ''}}" >
+    <input class="form-control" name="begin_date" type="date" id="begin_date" value="{{ isset($project->begin_date) ? $project->begin_date : date('Y-m-d') }}" >
     {!! $errors->first('begin_date', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('deadline') ? 'has-error' : ''}}">
@@ -26,7 +26,7 @@
 <div class="form-group {{ $errors->has('user_id') ? 'has-error' : ''}}">
     <label for="user_id" class="control-label">{{ 'User Id' }}</label>
     <input class="form-control" name="user_name" type="text" id="user_name" value="{{ isset($project->user_id) ? $project->user->name : Auth::user()->name }}" disabled >
-    <input class="form-control" name="user_id" type="hidden" id="user_id" value="{{ isset($project->user_id) ? $project->user_id : ''}}" >
+    <input class="form-control" name="user_id" type="hidden" id="user_id" value="{{ isset($project->user_id) ? $project->user_id : Auth::id() }}" >
     {!! $errors->first('user_id', '<p class="help-block">:message</p>') !!}
 </div>
 <div class="form-group {{ $errors->has('remark') ? 'has-error' : ''}}">
