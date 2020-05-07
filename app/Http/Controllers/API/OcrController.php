bfed33d3d103f5a2270f762f57f07e5bf3ac6b19
Chavalit Koweerawong <chavalit.kow@gmail.com>
2020-05-07 18:17:31 +0700

    Update OcrController.php

--- app/Http/Controllers/API/OcrController.php
+++ app/Http/Controllers/API/OcrController.php
@@ -283,7 +283,7 @@ class OcrController extends Controller
         $string_json = str_replace("<numbers>",join(",",json_decode($data["numbers"])),$string_json); 
         
         $string_json = str_replace("<title>",$data["title"],$string_json); 
-              
+              /*
         //5
         $n = $data['title'];        
         if(is_numeric($n)){            
@@ -299,7 +299,7 @@ class OcrController extends Controller
         $string_json = str_replace("<min5>",$levels[5],$string_json);
         $string_json = str_replace("<min6>",$levels[6],$string_json);
         $string_json = str_replace("<min7>",$levels[7],$string_json);
-        
+        */
         //6
         $string_json = str_replace("<user_id>",$event["source"]["userId"],$string_json);
         //7
